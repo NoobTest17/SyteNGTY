4eaa436d6a0d46dfa078993b21e5086251780ca2
Мигачев Максим Алексеевич <MigachevMA@lad.local>
2023-10-19 14:41:40 +0300

    Пробы с главной стр

--- src/components/Commons/Header/index.jsx
+++ src/components/Commons/Header/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import { Link } from 'react-router-dom';
-import { routes } from '../../utils/routes';
+import { routes } from '../../../utils/routes';
+import logo from '../../../assets/output.svg'
+
+
 
 export const Header = () => {
   return (
       <header className={styles.header}>
-        <div className={styles.logo}>История России</div>
+        <img src={logo} alt=''/>
         <nav className={styles.NavBar} >
           <ul className={styles.list}>
             <li><Link to={routes.main}>Главная страница</Link></li>
--- src/components/Commons/Layouts/index.jsx
+++ src/components/Commons/Layouts/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Header } from '../../Header';
-import { Footer } from '../../Footer';
+import { Header } from '../Header';
+import { Footer } from '../Footer';
 import styles from './Layouts.module.scss';
 
 export const Layouts = ({ children }) => {
