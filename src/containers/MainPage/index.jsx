40ece230f5ece8044b8b9fc4dc89a4c538c204ea
NoobTest17 <maximnnn.nnn@yandex.ru>
2023-10-25 00:41:07 +0300

    Сделать гланый текст более интересным и продожить работу над секциями с фактами

--- src/containers/MainPage/index.jsx
+++ src/containers/MainPage/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styles from './MainPage.module.scss';
-import { MyModal } from '../../components/Commons/MyModal/MyModal';
+import { MyModal } from '../../components/Commons/MyModal';
 import { mainArticles } from '../../utils/const/articlesMain';
 import { MyVideoPlayer } from '../../components/Commons/MyVideoPlayer';
 
@@ -19,44 +19,49 @@ export const MainPage = () => {
 
   return (
     <section className={styles.main}>
-      <div>
-        <MyVideoPlayer />
-      </div>
-      <article>
-        <h1>Роль женщин в Русской Революции 1917 года</h1>
-        <div className={styles.content}>
-          <p>
-            Добро пожаловать на наш сайт, посвященный истории и важной роли, которую женщины сыграли
-            в русской революции 1917 года. Этот период русской истории был временем перемен, борьбы
-            за равенство и социальную справедливость, и женщины стояли в первых рядах этого
-            движения.
-          </p>
+      {/*<div><MyVideoPlayer /></div>*/}
+
+      <article className={styles.presenterText}>
+        <h1 className={styles.presenterText__title}>Роль женщин в Русской Революции 1917 года</h1>
+        <div className={styles.presenterText__content}>
+          <div>
+            <p>
+              Добро пожаловать на наш сайт, посвященный истории и важной роли, которую женщины сыграли в русской
+              революции 1917 года.
+            </p>
+            <p>
+              Этот период русской истории был временем перемен, борьбы
+              за равенство и социальную справедливость, и женщины стояли
+              в первых рядах этого движения.
+            </p>
+          </div>
+
         </div>
       </article>
 
-      <article>
-        <h2>{mainArticles[0].title}</h2>
-        <div className={styles.content}>
-          <p>{mainArticles[0].shortText}</p>
-          <button onClick={() => openModal(0)}>Подробнее</button>
-          <MyModal isOpen={modalsIsOpen[0]} onRequestClose={closeModal}>
-            <h1>{mainArticles[0].title}</h1>
-            <p>{mainArticles[0].mainText}</p>
-          </MyModal>
-        </div>
-      </article>
+      {/*<article>*/}
+      {/*  <h2>{mainArticles[0].title}</h2>*/}
+      {/*  <div className={styles.content}>*/}
+      {/*    <p>{mainArticles[0].shortText}</p>*/}
+      {/*    <button onClick={() => openModal(0)}>Подробнее</button>*/}
+      {/*    <MyModal isOpen={modalsIsOpen[0]} onRequestClose={closeModal}>*/}
+      {/*      <h1>{mainArticles[0].title}</h1>*/}
+      {/*      <p>{mainArticles[0].mainText}</p>*/}
+      {/*    </MyModal>*/}
+      {/*  </div>*/}
+      {/*</article>*/}
 
-      <article>
-        <h1>{mainArticles[1].title}</h1>
-        <div className={styles.content}>
-          <p>{mainArticles[1].shortText}</p>
-          <button onClick={() => openModal(1)}>Подробнее</button>
-          <MyModal isOpen={modalsIsOpen[1]} onRequestClose={closeModal}>
-            <h1>{mainArticles[1].title}</h1>
-            <p>{mainArticles[1].mainText}</p>
-          </MyModal>
-        </div>
-      </article>
+      {/*<article>*/}
+      {/*  <h1>{mainArticles[1].title}</h1>*/}
+      {/*  <div className={styles.content}>*/}
+      {/*    <p>{mainArticles[1].shortText}</p>*/}
+      {/*    <button onClick={() => openModal(1)}>Подробнее</button>*/}
+      {/*    <MyModal isOpen={modalsIsOpen[1]} onRequestClose={closeModal}>*/}
+      {/*      <h1>{mainArticles[1].title}</h1>*/}
+      {/*      <p>{mainArticles[1].mainText}</p>*/}
+      {/*    </MyModal>*/}
+      {/*  </div>*/}
+      {/*</article>*/}
     </section>
   );
 };
