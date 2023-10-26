e75f4e8e0a2baed0a617a5b8596db46d6b120121
NoobTest17 <maximnnn.nnn@yandex,ru>
2023-10-26 17:45:28 +0300

    Items main page test

--- src/containers/MainPage/MainItem/index.jsx
+++ src/containers/MainPage/MainItem/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from './MainItem.module.scss'
 
-export const MainItem = ({title}) => {
+export const MainItem = ({ data }) => {
   return (
-    <div>
-
+    <div className={styles.item}>
+      <h1>title</h1>
+      <p>mainText</p>
     </div>
   );
 };
