119ff9843f9ab81da3a3059e43e060396e8df5c3
NoobTest17 <maximnnn.nnn@yandex,ru>
2023-10-27 16:04:40 +0300

    Items main page ( TEST (((( )

--- src/containers/MainPage/MainItem/index.jsx
+++ src/containers/MainPage/MainItem/index.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './MainItem.module.scss'
+import { MyModal } from '../../../components/Commons/MyModal';
 
-export const MainItem = ({ data }) => {
+export const MainItem = ({ data, children }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = (index) => {
+    setModalIsOpen(true);
+    document.body.style.overflow = 'hidden';
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    document.body.style.overflow = 'auto';
+  };
+
   return (
-    <div className={styles.item}>
-      <h1>title</h1>
-      <p>mainText</p>
+    <article>
+    <h2>{}</h2>
+    <div className={styles.content}>
+      <p>{}</p>
+      <button onClick={openModal}>Подробнее</button>
+      <MyModal isOpen={modalIsOpen} onRequestClose={closeModal}>
+        {children}
+      </MyModal>
     </div>
+  </article>
   );
 };
